dce377a330e8bbae99f5b959f21e4d9c09665c02
ojh31 <oskar.hollinsworth@gmail.com>
2023-06-23 14:58:36 +0100

    Did positional version of resample ablation

--- resample_ablation.py
+++ resample_ablation.py
@@ -13,7 +13,7 @@ from collections import defaultdict
 from tqdm import tqdm
 from path_patching import act_patch, Node, IterNode
 #%% # Model loading
-device ='cpu' # torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device('cpu')
 model = HookedTransformer.from_pretrained(
     "gpt2-small",
     center_unembed=True,
@@ -21,10 +21,17 @@ model = HookedTransformer.from_pretrained(
     fold_ln=True,
     device=device,
 )
+
 #%%
 all_prompts, answer_tokens, clean_tokens, corrupted_tokens = get_dataset(
     model, device
 )
+#%%
+example_prompt = model.to_str_tokens(clean_tokens[0])
+adj_token = example_prompt.index(' perfect')
+verb_token = example_prompt.index(' loved')
+s2_token = example_prompt.index(' movie', example_prompt.index(' movie') + 1)
+end_token = len(example_prompt) - 1
 # %%
 name_filter = lambda name: name.endswith('z')
 clean_logits, clean_cache = model.run_with_cache(
@@ -37,6 +44,19 @@ corrupted_logits, corrupted_cache = model.run_with_cache(
 )
 corrupted_logit_diff = get_logit_diff(corrupted_logits, answer_tokens, per_prompt=False)
 print('corrupted logit diff', corrupted_logit_diff)
+#%%
+clean_cache = ActivationCache(
+    {k: v.to(device) for k, v in clean_cache.items()},
+    model,
+    has_batch_dim=clean_cache.has_batch_dim,
+)
+corrupted_cache = ActivationCache(
+    {k: v.to(device) for k, v in corrupted_cache.items()},
+    model,
+    has_batch_dim=corrupted_cache.has_batch_dim,
+)
+#%%
+assert clean_cache['blocks.0.attn.hook_z'].device == device
 #%%
 def logit_diff_denoising(
     logits: Float[Tensor, "batch seq d_vocab"],
@@ -52,6 +72,12 @@ def logit_diff_denoising(
     patched_logit_diff = get_logit_diff(logits, answer_tokens)
     return ((patched_logit_diff - flipped_logit_diff) / (clean_logit_diff  - flipped_logit_diff)).item()
 #%%
+# ============================================================================ #
+# Node resample ablation
+
+tokenizer_heads = [
+    (0, 4),
+]
 dae_heads = [
     (7, 1),
     (9, 2),
@@ -69,7 +95,7 @@ iam_heads = [
     (7, 1),
     (7, 5),
 ]
-circuit_heads = dae_heads + iae_heads + iam_heads
+circuit_heads = dae_heads + iae_heads + iam_heads + tokenizer_heads
 non_circuit_heads = [
     (layer, head)
     for layer in range(model.cfg.n_layers)
@@ -114,3 +140,77 @@ print(
 )
 
 # %%
+# ============================================================================ #
+# Positional resample ablation
+tokenizer_heads = [
+    (0, 4, adj_token),
+    (0, 4, verb_token),
+]
+dae_heads = [
+    (7, 1, end_token),
+    (9, 2, end_token),
+    (10, 1, end_token),
+    (10, 4, end_token),
+    (11, 9, end_token),
+]
+iae_heads = [
+    (8, 5, end_token),
+    (9, 2, end_token),
+    (9, 10, end_token),
+]
+iam_heads = [
+    (6, 4, s2_token),
+    (7, 1, s2_token),
+    (7, 5, s2_token),
+]
+circuit_heads = dae_heads + iae_heads + iam_heads + tokenizer_heads
+non_circuit_heads = [
+    (layer, head, pos)
+    for layer in range(model.cfg.n_layers)
+    for head in range(model.cfg.n_heads)
+    for pos in range(len(example_prompt))
+    if (layer, head, pos) not in circuit_heads
+]
+circuit_nodes = [
+    Node("z", layer=node[0], head=node[1], seq_pos=node[2]) 
+    for node in circuit_heads
+]
+non_circuit_nodes = [
+    Node("z",  layer=node[0], head=node[1], seq_pos=node[2]) 
+    for node in non_circuit_heads
+]
+#%%
+print(
+    "unpatched logit diff",
+    logit_diff_denoising(logits=corrupted_logits)
+)
+# %%
+non_circuit_results = act_patch(
+    model=model,
+    orig_input=corrupted_tokens,
+    new_cache=clean_cache,
+    patching_nodes=non_circuit_nodes,
+    patching_metric=logit_diff_denoising,
+    verbose=True,
+)
+print(
+    f'logit diff from patching non-circuit positions/nodes: {non_circuit_results:.0%}'
+    )
+# %%
+circuit_results = act_patch(
+    model=model,
+    orig_input=corrupted_tokens,
+    new_cache=clean_cache,
+    patching_nodes=circuit_nodes,
+    patching_metric=logit_diff_denoising,
+    verbose=True,
+)
+print(
+    f'logit diff from patching circuit positions/nodes: {circuit_results:.0%}')
+
+# %%
+print(
+    "fully patched logit diff",
+    logit_diff_denoising(logits=clean_logits)
+)
+#%%
