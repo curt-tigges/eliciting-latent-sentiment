3986d55e16502d08a1ec08de7a5ed5d2a768350c
curt-tigges <ct@curttigges.com>
2023-06-28 14:28:50 -0400

    Added new prompts

--- utils/prompts.py
+++ utils/prompts.py
@@ -77,7 +77,7 @@ def get_prompts(
     negative_adjectives: list = neg_adj,
     neutral_adjectives: list = neutral_adj,
 ) -> Tuple[list, list]:
-    assert prompt_type in ["simple", "completion", "completion_2", "classification", "classification_2", "classification_3", "classification_4"]
+    assert prompt_type in ["simple", "completion", "completion_2", "completion_3", "classification", "classification_2", "classification_3", "classification_4"]
     if prompt_type == "simple":
         pos_prompts = [
             f"I thought this movie was{positive_adjectives[i]}, I loved it. \nConclusion: This movie is" for i in range(len(positive_adjectives))
@@ -108,6 +108,16 @@ def get_prompts(
         ]
         neutral_prompts = [
             f"I thought this movie was{get_adjective(neutral_adjectives, i)}, I watched it. The acting was{get_adjective(neutral_adjectives, i+1)}, the plot was{get_adjective(neutral_adjectives, i+2)}, and overall it was just very average. I felt it was" for i in range(len(positive_adjectives))
+        ]
+    elif prompt_type == "completion_3":
+        pos_prompts = [
+            f"I thought this movie was{get_adjective(positive_adjectives, i)}, I loved it. The acting was{get_adjective(positive_adjectives, i+1)}, the plot was{get_adjective(positive_adjectives, i+2)}, and overall it was just very good. This film was really" for i in range(len(positive_adjectives))
+        ]
+        neg_prompts = [
+            f"I thought this movie was{get_adjective(negative_adjectives, i)}, I hated it. The acting was{get_adjective(negative_adjectives, i+1)}, the plot was{get_adjective(negative_adjectives, i+2)}, and overall it was just very bad. This film was really" for i in range(len(positive_adjectives))
+        ]
+        neutral_prompts = [
+            f"I thought this movie was{get_adjective(neutral_adjectives, i)}, I watched it. The acting was{get_adjective(neutral_adjectives, i+1)}, the plot was{get_adjective(neutral_adjectives, i+2)}, and overall it was just very average. This film was really" for i in range(len(positive_adjectives))
         ]
     elif prompt_type == "classification":
         pos_prompts = [
@@ -176,6 +186,87 @@ def get_dataset(
         in the format (correct_token, incorrect_token)
     '''
     assert n_pairs <= len(pos_tokens)
+    
+    if "pythia" in model.cfg.model_name:
+        positive_adjectives = remove_pythia_double_token_words(model, pos_adj)
+        negative_adjectives = remove_pythia_double_token_words(model, neg_adj)
+        neutral_adjectives = remove_pythia_double_token_words(
+            model, neutral_adj
+        )
+        pos_answers = remove_pythia_double_token_words(model, pos_answers)
+        neg_answers = remove_pythia_double_token_words(model, neg_answers)
+        neutral_answers = remove_pythia_double_token_words(model, neutral_answers)
+    else:
+        positive_adjectives = pos_adj
+        negative_adjectives = neg_adj
+        neutral_adjectives = neutral_adj
+
+    pos_prompts, neg_prompts, neutral_prompts = get_prompts(
+        prompt_type, positive_adjectives, negative_adjectives, neutral_adjectives
+    )
+    prompts_dict = {
+        "positive": pos_prompts,
+        "negative": neg_prompts,
+        "neutral": neutral_prompts,
+    }
+    n_prompts = min(
+        len(prompts_dict[comparison[0]]), 
+        len(prompts_dict[comparison[1]]), 
+        )
+    batch_size = n_prompts * 2
+    all_prompts = []
+    answer_tokens = torch.empty(
+        (batch_size, n_pairs, 2), 
+        device=device, 
+        dtype=torch.long
+    )
+    
+    for i in range(n_prompts):
+
+        all_prompts.append(prompts_dict[comparison[0]][i])
+        all_prompts.append(prompts_dict[comparison[1]][i])
+        
+        for pair_idx in range(n_pairs):
+            pos_token = model.to_single_token(pos_answers[pair_idx])
+            neg_token = model.to_single_token(neg_answers[pair_idx])
+            neut_token = model.to_single_token(neutral_answers[pair_idx])
+            tokens_dict = {
+                'positive': pos_token, 
+                'negative': neg_token, 
+                'neutral': neut_token,
+            }
+            answer_tokens[i * 2, pair_idx, 0] = tokens_dict[comparison[0]]
+            answer_tokens[i * 2, pair_idx, 1] = tokens_dict[comparison[1]]
+            answer_tokens[i * 2 + 1, pair_idx, 0] = tokens_dict[comparison[1]]
+            answer_tokens[i * 2 + 1, pair_idx, 1] = tokens_dict[comparison[0]]
+    
+    prompts_tokens: Float[Tensor, "batch pos"] = model.to_tokens(
+        all_prompts, prepend_bos=True
+    )
+    
+    clean_tokens = prompts_tokens.to(device)
+    corrupted_tokens = model.to_tokens(
+        all_prompts[1:] + [all_prompts[0]], prepend_bos=True
+    ).to(device)
+    
+    return (
+        all_prompts, answer_tokens, clean_tokens, corrupted_tokens
+    )
+
+
+def get_task_contrast_dataset(
+    model: HookedTransformer, 
+    device: torch.device,
+    n_pairs: int = 1,
+    prompt_type: str = "contrastive_tasks",
+    answers_dict: dict = {},
+) -> Tuple[
+    Float[Tensor, "batch pos"],
+    Float[Tensor, "batch n_pairs 2"],
+    Float[Tensor, "batch pos"],
+    Float[Tensor, "batch pos"],
+]:
+    assert set(answers_dict.keys()) == set(["task_1_positive", "task_1_negative", "task_2_positive", "task_2_negative"])
     
     if "pythia" in model.cfg.model_name:
         positive_adjectives = remove_pythia_double_token_words(model, pos_adj)
