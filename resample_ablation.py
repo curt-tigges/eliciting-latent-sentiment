177a1030b3f87161ae8e0897607af3695da20351
ojh31 <oskar.hollinsworth@gmail.com>
2023-06-29 15:14:17 +0100

    Attemped to implement directional patching but not getting good results

--- resample_ablation.py
+++ resample_ablation.py
@@ -4,7 +4,8 @@ import numpy as np
 from jaxtyping import Float, Int
 import plotly.express as px
 from utils.prompts import get_dataset
-from utils.circuit_analysis import get_logit_diff_multi as get_logit_diff
+from utils.circuit_analysis import get_logit_diff
+from utils.store import save_array, load_array
 import torch
 from torch import Tensor
 from transformer_lens import ActivationCache, HookedTransformer, HookedTransformerConfig, utils
@@ -15,15 +16,18 @@ from tqdm import tqdm
 from path_patching import act_patch, Node, IterNode
 #%% # Model loading
 device = torch.device('cpu')
+MODEL_NAME = 'gpt2-small'
 model = HookedTransformer.from_pretrained(
-    "gpt2-small",
+    MODEL_NAME,
     center_unembed=True,
     center_writing_weights=True,
     fold_ln=True,
     device=device,
 )
-
+model.cfg.use_attn_result = True
+model.name = MODEL_NAME
 #%%
+# corrupted to clean patching style
 all_prompts, answer_tokens, clean_tokens, corrupted_tokens = get_dataset(
     model, device
 )
@@ -34,20 +38,19 @@ verb_token = example_prompt.index(' loved')
 s2_token = example_prompt.index(' movie', example_prompt.index(' movie') + 1)
 end_token = len(example_prompt) - 1
 # %%
-name_filter = lambda name: name.endswith('z')
 clean_logits, clean_cache = model.run_with_cache(
-    clean_tokens, names_filter=name_filter
+    clean_tokens, names_filter=lambda name: name.endswith('z')
 )
 clean_logit_diff = get_logit_diff(clean_logits, answer_tokens, per_prompt=False)
 print('clean logit diff', clean_logit_diff)
 corrupted_logits, corrupted_cache = model.run_with_cache(
-    corrupted_tokens, names_filter=name_filter
+    corrupted_tokens, names_filter=lambda name: name.endswith('z')
 )
 corrupted_logit_diff = get_logit_diff(corrupted_logits, answer_tokens, per_prompt=False)
 print('corrupted logit diff', corrupted_logit_diff)
 #%%
-clean_cache = clean_cache.to(device)
-corrupted_cache = corrupted_cache.to(device)
+clean_cache.to(device)
+corrupted_cache.to(device)
 #%%
 assert clean_cache['blocks.0.attn.hook_z'].device == device
 #%%
@@ -66,7 +69,7 @@ def logit_diff_denoising(
     return ((patched_logit_diff - flipped_logit_diff) / (clean_logit_diff  - flipped_logit_diff)).item()
 #%%
 # ============================================================================ #
-# Node resample ablation
+# Head resample ablation
 
 tokenizer_heads = [
     (0, 4),
@@ -201,6 +204,130 @@ circuit_results = act_patch(
 print(
     f'logit diff from patching circuit positions/nodes: {circuit_results:.0%}')
 
+# %%
+print(
+    "fully patched logit diff",
+    logit_diff_denoising(logits=clean_logits)
+)
+#%%
+# ============================================================================ #
+# Directional resample ablation
+pos_dir = load_array("km_2c_line_embed_and_mlp0", model)
+pos_dir /= np.linalg.norm(pos_dir)
+pos_dir = torch.tensor(pos_dir, device=device, dtype=torch.float32)
+neg_dir = load_array("rotation_direction0", model)
+neg_dir /= np.linalg.norm(neg_dir)
+neg_dir = torch.tensor(neg_dir, device=device, dtype=torch.float32)
+directions = [pos_dir, neg_dir]
+#%%
+clean_logits, clean_cache = model.run_with_cache(
+    clean_tokens, names_filter=lambda name: name.endswith('result') or name ==  'blocks.0.attn.hook_z'
+)
+corrupted_logits, corrupted_cache = model.run_with_cache(
+    corrupted_tokens, names_filter=lambda name: name.endswith('result') or name ==  'blocks.0.attn.hook_z'
+)
+clean_cache.to(device)
+corrupted_cache.to(device)
+
+#%%
+def create_directional_cache(directions: List[np.ndarray]) -> ActivationCache:
+    cache = {}
+    for act_name, orig_value in corrupted_cache.items():
+        new_value = clean_cache[act_name]
+        for direction in directions:
+            if not act_name.endswith('result'):
+                cache[act_name] = orig_value
+                continue
+            orig_proj = einops.einsum(
+                orig_value, 
+                direction, 
+                "batch pos head d_model, d_model -> batch pos head"
+            )
+            new_proj = einops.einsum(
+                new_value, 
+                direction, 
+                "batch pos head d_model, d_model -> batch pos head"
+            )
+            proj_diff_rep: Float[Tensor, "batch pos head d_model"] = einops.repeat(
+                new_proj - orig_proj, 
+                "batch pos head -> batch pos head d_model", 
+                d_model=model.cfg.d_model
+            )
+            orig_value += proj_diff_rep * direction
+        cache[act_name] = orig_value
+    return ActivationCache(cache, model)
+
+direction_cache = create_directional_cache(directions)
+#%%
+tokenizer_heads = [
+    (0, 4),
+]
+dae_heads = [
+    (7, 1),
+    (9, 2),
+    (10, 1),
+    (10, 4),
+    (11, 9),
+]
+iae_heads = [
+    (8, 5),
+    (9, 2),
+    (9, 10),
+]
+iam_heads = [
+    (6, 4),
+    (7, 1),
+    (7, 5),
+]
+circuit_heads = dae_heads + iae_heads + iam_heads + tokenizer_heads
+non_circuit_heads = [
+    (layer, head)
+    for layer in range(model.cfg.n_layers)
+    for head in range(model.cfg.n_heads)
+    if (layer, head) not in circuit_heads
+]
+circuit_nodes = [Node("result", *node) for node in circuit_heads]
+non_circuit_nodes = [Node("result", *node) for node in non_circuit_heads]
+#%%
+print(
+    "unpatched logit diff",
+    logit_diff_denoising(logits=corrupted_logits)
+)
+# %%
+non_circuit_results = act_patch(
+    model=model,
+    orig_input=corrupted_tokens,
+    new_cache=direction_cache,
+    patching_nodes=non_circuit_nodes,
+    patching_metric=logit_diff_denoising,
+    verbose=True,
+)
+print(
+    f'logit diff from patching non-circuit directions/nodes: {non_circuit_results:.1%}'
+    )
+# %%
+circuit_results = act_patch(
+    model=model,
+    orig_input=corrupted_tokens,
+    new_cache=direction_cache,
+    patching_nodes=circuit_nodes,
+    patching_metric=logit_diff_denoising,
+    verbose=True,
+)
+print(
+    f'logit diff from patching circuit directions/nodes: {circuit_results:.1%}')
+#%%
+full_direction_results = act_patch(
+    model=model,
+    orig_input=corrupted_tokens,
+    new_cache=direction_cache,
+    patching_nodes=circuit_nodes + non_circuit_nodes,
+    patching_metric=logit_diff_denoising,
+    verbose=True,
+)
+print(
+    f'logit diff from patching direction at all nodes: {full_direction_results:.1%}'
+)
 # %%
 print(
     "fully patched logit diff",
