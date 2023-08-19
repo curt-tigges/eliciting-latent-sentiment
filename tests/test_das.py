2a6666b78a664c3d7b02f5638fd0fbbe3bc69a7e
ojh31 <oskar.hollinsworth@gmail.com>
2023-08-18 21:19:29 -0700

    Added some basic gpt4 unit tests for das

---
+++ tests/test_das.py
@@ -0,0 +1,57 @@
+import unittest
+import torch
+from transformer_lens.hook_points import HookPoint
+from utils.das import RotateLayer, InverseRotateLayer, hook_fn_base, act_patch_simple, RotationModule, TrainingConfig
+
+class TestFunctions(unittest.TestCase):
+
+    def test_rotate_layer(self):
+        layer = RotateLayer(5, torch.device('cpu'))
+        x = torch.randn(3, 5)
+        y = layer(x)
+        self.assertEqual(y.shape, x.shape)
+
+    def test_inverse_rotate_layer(self):
+        layer = RotateLayer(5, torch.device('cpu'))
+        x = torch.randn(3, 5)
+        inv_layer = InverseRotateLayer(layer)
+        y = inv_layer(layer(x))
+        self.assertTrue(torch.allclose(y, x))
+
+    def test_hook_fn_base(self):
+        resid = torch.ones(3, 4, 5)
+        layer = 0
+        hook = HookPoint()
+        hook.layer = lambda: layer
+        hook.name = f'blocks.{layer}.resid_pre'
+        new_value = torch.full((3, 5), 2)
+        out = hook_fn_base(resid, hook, 0, 1, new_value)
+        self.assertEqual(out[0, 1, 0], 2)
+        self.assertEqual(out[0, 0, 0], 1)
+
+    def test_act_patch_simple(self):
+        layer = 10
+        model = unittest.mock.MagicMock()
+        model.run_with_hooks.return_value = torch.ones(3, 10)
+        model.cfg.n_layers = layer
+        orig_input = "test"
+        new_value = torch.full((3, 5), 2)
+        patching_metric = lambda x: torch.sum(x)
+        out = act_patch_simple(model, orig_input, new_value, layer, 1, patching_metric)
+        self.assertEqual(out.item(), 30)
+
+    def test_training_config(self):
+        config_dict = {
+            "seed": 42,
+            "lr": 0.001,
+            "epochs": 10,
+            "n_directions": 2,
+            "wandb_enabled": False
+        }
+        config = TrainingConfig(config_dict)
+        self.assertEqual(config.seed, 42)
+        self.assertEqual(config.to_dict(), config_dict)
+
+
+if __name__ == "__main__":
+    unittest.main()
