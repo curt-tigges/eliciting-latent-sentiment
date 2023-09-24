4f3d00cea29ec8ad77f9687efb39a32295dc015b
ojh31 <oskar.hollinsworth@gmail.com>
2023-09-24 10:12:05 +0100

    Removed treebank from cosine sims

--- compare_lines.py
+++ compare_lines.py
@@ -6,7 +6,7 @@ import plotly.express as px
 import re
 import os
 import pandas as pd
-from utils.store import load_array, save_html, extract_layer_from_string, zero_pad_layer_string
+from utils.store import load_array, save_html, extract_layer_from_string, zero_pad_layer_string, flatten_multiindex
 # %%
 MODEL_NAME = "gpt2-small"
 PATTERN = (
@@ -30,7 +30,7 @@ def parse_filename(filename):
 #%%
 direction_labels = get_directions()
 direction_layers = [extract_layer_from_string(label) for label in direction_labels]
-directions = [load_array(filename, MODEL_NAME) for filename in direction_labels]
+directions = [load_array(filename, MODEL_NAME).squeeze() for filename in direction_labels]
 direction_labels = [zero_pad_layer_string(label) for label in direction_labels]
 label_tuples = [parse_filename(label) for label in direction_labels]
 treebank_layers = [layer for i, layer in enumerate(direction_layers) if label_tuples[i][1] == 'treebank_train']
@@ -40,17 +40,6 @@ stacked = (stacked.T / np.linalg.norm(stacked, axis=1)).T
 similarities: Float[np.ndarray, "direction d_model"] = einops.einsum(
     stacked, stacked, 'm d, n d -> m n'
 )
-#%%
-def flatten_multiindex(df):
-    # Flatten columns
-    if isinstance(df.columns, pd.MultiIndex):
-        df.columns = ['_'.join(map(str, col)).strip() for col in df.columns.values]
-    
-    # Flatten index if it's a multiindex
-    if isinstance(df.index, pd.MultiIndex):
-        df.index = ['_'.join(map(str, idx)).strip() for idx in df.index.values]
-    
-    return df
 #%%
 def move_col_to_end(col, df):
     cols = list(df.columns)
@@ -63,6 +52,20 @@ def move_row_to_end(row, df):
     rows.remove(row)
     rows.append(row)
     return df.loc[rows]
+#%%
+def clean_labels(labels: pd.Series):
+    return (
+        labels
+        .str.replace("simple_train_", "")
+        .str.replace("logistic_regression", "LR")
+        .str.replace("das", "DAS")
+        .str.replace("kmeans", "K_means")
+        .str.replace("pca", "PCA")
+        .str.replace("treebank_train_ALL", "treebank")
+        .str.replace("_0", "")
+        .str.replace("mean_diff", "Mean_diff")
+        .str.replace("_ADJ", "")
+    )
 # %%
 df = pd.DataFrame(
     similarities,
@@ -87,11 +90,16 @@ df = df.loc[
 ]
 df = flatten_multiindex(df)
 df = df.abs()
-df = move_col_to_end('das_treebank_train_ALL_0', df)
-df = move_row_to_end('das_treebank_train_ALL_0', df)
-df.columns = df.columns.str.replace("_train", "").str.replace("logistic_regression", "lr")
-df.index = df.index.str.replace("_train", "").str.replace("logistic_regression", "lr")
-styled = df.style.background_gradient(cmap='Reds').format("{:.1%}")
-save_html(styled, 'direction_similarities', MODEL_NAME, static=True)
+# df = df.drop(columns=['das_treebank_train_ALL_0'], index=['das_treebank_train_ALL_0'])
+df.columns = clean_labels(df.columns)
+df.index = clean_labels(df.index)
+styled = (
+    df.style
+    .background_gradient(cmap='Reds', vmin=0, vmax=1)
+    .format("{:.1%}")
+)
+save_html(
+    styled, 'direction_similarities', MODEL_NAME, static=True
+)
 styled
 # %%
