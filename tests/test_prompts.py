dc7277ec2c9f27a89e0fc0b7ee1af7ed4a4ac997
ojh31 <oskar.hollinsworth@gmail.com>
2023-09-25 12:58:45 +0100

    Fixed setUp in test cache results

--- tests/test_prompts.py
+++ tests/test_prompts.py
@@ -67,10 +67,6 @@ class TestCleanCorruptedCacheResults(unittest.TestCase):
             [0.6],
             [0.7],
             [0.8],
-            0.5, 
-            0.6, 
-            0.7, 
-            0.8,
         )
 
     def test_initialization(self):
